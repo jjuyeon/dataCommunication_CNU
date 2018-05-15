cbab040f7e9dc8ea4fd2dd1168e0288338e1a89e
Suyeon Jin <gmlakd8944@cnu.ac.kr>
2018-05-15 20:18:00 +0900

    implement error detection and correction(NAK transmission and discard) , check 'checksum error test, timeout error test'

--- stop-and-wait-ARQ/receiver.py
+++ stop-and-wait-ARQ/receiver.py
@@ -2,9 +2,15 @@ import socket
 import struct
 import os
 import hashlib
+import time
 
 ip_address = '127.0.0.1'
 port_number = 3333
+
+def make_checksum(data): #checksum을 생성하는 함수
+	checksum = hashlib.sha1() #160bit 해시 암호
+	checksum.update(data)
+	return checksum
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_sock.bind((ip_address, port_number))
@@ -14,41 +20,51 @@ print("Send file info ACK..")
 
 while True:
 	data,addr = server_sock.recvfrom(1045)
-	recv_checksum = data[0:20] #받은 암호 저장
-	recv_data = data[20:]
+	output_checksum = data[0:20] #받은 암호 저장
+	output_data = data[20:]
 
 	#받은 데이터 checksum 생성
-	data_checksum = hashlib.sha1() #160bit 해시 암호
-	data_checksum.update(recv_data)
-	
-	seqNum = recv_data[0]
-	if (recv_checksum == data_checksum.digest()) and (seqNum == 0):
-		file_name = recv_data[1:12].decode()
-		file_size = struct.unpack("!I",recv_data[12:16])[0]
+	data_checksum = make_checksum(output_data)
+
+	output_seqNum = output_data[0]
+	if (output_checksum == data_checksum.digest()) and (output_seqNum == 0): #정상전송
+		file_name = output_data[1:12].decode()
+		file_size = struct.unpack("!I",output_data[12:16])[0]
+		file_path = "./received_dir/"
+		
+		if not os.path.isdir("received_dir/"): #디렉토리 유무 확인
+			print("received_dir 디렉토리가 없으므로 생성합니다.")
+			os.mkdir("received_dir/") #없으면 생성
+
+		file = open(file_path+file_name,"wb")
+
 		print("File Name = ",file_name)
 		print("File Size = ",file_size)
+		print("received file Path = ",file_path+file_name)
 
-		if seqNum % 2 == 0:
+		if output_seqNum % 2 == 0:
 			ACK=1
-		elif seqNum % 2 == 1:
+		elif output_seqNum % 2 == 1:
 			ACK=0
 		server_sock.sendto(ACK.to_bytes(1,byteorder="big"), addr)
-		prev_seqNum = seqNum #seqNum검사 위해 저장
+		prev_seqNum = output_seqNum #seqNum검사 위해 저장
 		break #정확한 정보를 받으면 종료
 	
-	#elif seqNum == 1: #seqNum 잘못된 경우:NAK전송 (다음주구현)
-	#elif recv_checksum != data_checksum.digest(): #checksum 잘못된 경우:NAK전송 (다음주구현)
+	elif output_seqNum == 1: #seqNum 잘못된 경우:ACK전송 + packet discard
+		if output_seqNum % 2 == 0:
+			ACK=1
+		elif output_seqNum % 2 == 1:
+			ACK=0
+		server_sock.sendto(ACK.to_bytes(1,byteorder="big"), addr)
 
-#파일 데이터 받기	
-output_path = "./received_dir/"
+	elif output_checksum != data_checksum.digest(): #checksum 잘못된 경우:NAK전송
+		print("* Packet corrupted!! *** - Send To Sender NAK(2)")
+		NAK=2
+		server_sock.sendto(NAK.to_bytes(1,byteorder="big"), addr)
 
-if not os.path.isdir("received_dir/"): #디렉토리 유무 확인
-	print("received_dir 디렉토리가 없으므로 생성합니다.")
-	os.mkdir("received_dir/") #없으면 생성
-
-file = open(output_path+file_name,"wb")
+#파일 데이터 받기
 current_size = 0 #만들어지는 파일의 현재 상태 확인하기 위한 변수
-
+receive_count = 0 #오류검사를 하기 위한 변수
 while True:
 	total_message,addr = server_sock.recvfrom(1045)
 	output_checksum = total_message[0:20] #checksum
@@ -56,13 +72,18 @@ while True:
 	output_message = total_message[21:]
 
 	#받은 데이터 checksum 생성
-	data_checksum = hashlib.sha1() #160bit 해시 암호
-	data_checksum.update(total_message[20:])
-	
-	if(output_checksum == data_checksum.digest()) and (prev_seqNum != output_seqNum):
+	data_checksum = make_checksum(total_message[20:])
+
+	if(output_checksum == data_checksum.digest()) and (prev_seqNum != output_seqNum): #정상전송
 		#파일 쓰기 시작
 		file.write(output_message)
 		current_size += len(output_message)
+		receive_count += 1 #파일을 몇번 receive하는지 저장
+
+		if (receive_count == 20) or (receive_count == 25):#timeout error 검사하기 위함 (20번째,25번째 전송일 때 검사)
+			print("wait for 5...")
+			time.sleep(5)
+
 		resend_message = "(current size / total size) = "+ str(current_size)+ "/"+ str(file_size)+ " , "+  str(round(100 * (current_size / file_size), 3))+ " %"
 		print(resend_message)
 		
@@ -70,14 +91,26 @@ while True:
 			ACK=1
 		elif output_seqNum % 2 == 1:
 			ACK=0
-		server_sock.sendto(ACK.to_bytes(1,byteorder="big"), addr) 
+
+		server_sock.sendto(ACK.to_bytes(1,byteorder="big"), addr) #ACK전송
 		prev_seqNum = output_seqNum
-		
-	#elif prev_seqNum == output_seqNum: #seqNum 잘못된 경우:NAK전송 (다음주구현)
-	#elif output_checksum != data_checksum.digest(): #checksum 잘못된 경우:NAK전송 (다음주구현)
+
+	elif prev_seqNum == output_seqNum: #seqNum 잘못된 경우:ACK전송 + packet discard
+		if output_seqNum % 2 == 0:
+			ACK=1
+		elif output_seqNum % 2 == 1:
+			ACK=0
+
+		server_sock.sendto(ACK.to_bytes(1,byteorder="big"), addr)
 	
+	elif output_checksum != data_checksum.digest(): #checksum 잘못된 경우:NAK전송
+		print("* Packet corrupted!! *** - Send To Sender NAK(2)")
+		NAK = 2
+		server_sock.sendto(NAK.to_bytes(1,byteorder="big"), addr)
+
 	if current_size==file_size : #종료조건
 		break
 
 file.close()
 server_sock.close()
+print("File receive end.")
--- stop-and-wait-ARQ/sender.py
+++ stop-and-wait-ARQ/sender.py
@@ -6,7 +6,13 @@ import hashlib
 serverIP = '127.0.0.1'
 serverPort = 3333
 
+def make_checksum(data): #checksum을 생성하는 함수
+	checksum = hashlib.sha1() #160bit 해시 암호
+	checksum.update(data)
+	return checksum
+
 clnt_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP통신
+clnt_sock.settimeout(3) #응답시간 제한 설정(3초)
 
 print("Sender Socket open..")
 print("Receiver IP = ",serverIP)
@@ -14,7 +20,7 @@ print("Receiver Port = ", serverPort)
 print("Send File Info(file Name, file Size, seqNum) to Server...")
 
 #파일 정보(파일명,파일크기) 전송 : 초기설정
-file_name = "ex.jpeg"
+file_name = "ex.jpg"
 
 # file open for read
 file = open("./"+file_name, "rb")
@@ -26,48 +32,77 @@ seqNum = 0 #초기설정(초기 seqNum는 0으로 설정)
 total_data = seqNum.to_bytes(1,byteorder = "big") + file_name.encode() + file_size.to_bytes(4,byteorder = "big")
 
 #checksum 생성
-checksum = hashlib.sha1() #160bit 해시 암호
-checksum.update(total_data)
+checksum = make_checksum(total_data)
 
 #sending
 while True:
-	send_message = checksum.digest()+total_data
-	clnt_sock.sendto(send_message, (serverIP, serverPort))
-	currentACK = clnt_sock.recv(1)[0]
-
-	if currentACK == 1:
-		prevACK = currentACK
+	clnt_sock.sendto(checksum.digest()+total_data, (serverIP, serverPort))
+	current_response = clnt_sock.recv(1)[0]
+	
+	if current_response == 1:
+		prev_response = current_response
+		prev_seqNum = seqNum
 		break
 
 #파일 데이터 읽어서 보내기
 print("Start File send")
 current_size = 0 #만들어지는 파일의 현재 상태 확인하기 위한 변수
+send_count = 0 #오류검사를 하기 위한 변수
 
 while True:
-	if currentACK % 2 == 0:
-		seqNum = 0
-	elif currentACK % 2 == 1:
-		seqNum = 1
+	try:
+		if (current_response == 0) or (current_response == 1): #ACK일때 전송
+			if current_response % 2 == 0:
+				seqNum = 0
+			elif current_response % 2 == 1: 
+				seqNum = 1
 
-	send_data = file.read(1024) #파일을 읽음
-	if not send_data : #종료조건
-		break
-
-	#checksum 생성
-	checksum = hashlib.sha1() #160bit 해시 암호
-	checksum.update(seqNum.to_bytes(1,byteorder="big") + send_data)
-
-	send_message = checksum.digest()+seqNum.to_bytes(1,byteorder="big") + send_data
-	clnt_sock.sendto(send_message, (serverIP, serverPort))
+			if (prev_seqNum != seqNum): #정상 전송
+				send_data = file.read(1024) #파일을 읽음
+				if not send_data: #종료조건
+					break
+				send_message = seqNum.to_bytes(1,byteorder="big") + send_data
+				
+				#checksum 생성
+				checksum = make_checksum(send_message)
 	
-	current_size += len(send_data)
-	resend_message = "(current size / total size) = "+ str(current_size)+ "/"+ str(file_size)+ " , "+  str(round(100 * (current_size / file_size), 3))+ " %"
-	print(resend_message)
+				if (send_count == 9) or (send_count == 14): #checksum error 검사하기 위함 (10, 15번째 전송일 때 검사)
+					addError = "error"
+					checksum = make_checksum(send_message + addError.encode())
+	
+				clnt_sock.sendto(checksum.digest()+send_message, (serverIP, serverPort))			
+				send_count+=1 #파일을 몇번 send하는지 저장
+				current_size += len(send_data)
+				resend_message = "(current size / total size) = "+ str(current_size)+ "/"+ str(file_size)+ " , "+  str(round(100 * (current_size / file_size), 3))+ " %"
+				
+				print(resend_message)
+				prev_response = current_response
+				prev_seqNum = seqNum
+				current_response = clnt_sock.recv(1)[0]	
 
-	currentACK = clnt_sock.recv(1)[0]		
-	prevACK = currentACK
+			elif (prev_response == current_response): #ACK받고 아무것도 하지않음 (empty)
+				prev_response =current_response			
+				current_response = clnt_sock.recv(1)[0]
+				continue
+
+		elif current_response == 2: #NAK일때 전송
+			print("* Received NAK - Retransmit!")
+			checksum = make_checksum(send_message) #checksum 다시 검사
+			clnt_sock.sendto(checksum.digest()+send_message, (serverIP, serverPort)) #데이터 재전송
+			print("Retransmission : "+resend_message)
+			prev_response = current_response
+			prev_seqNum = seqNum
+			current_response = clnt_sock.recv(1)[0]
 	
-	
+	except socket.timeout: #timeout 걸렸을 때
+		print("* TimeOut!! **")
+		clnt_sock.sendto(checksum.digest()+send_message, (serverIP, serverPort)) #데이터 재전송
+		print("Retransmission : "+resend_message)
+		prev_response = current_response
+		prev_seqNum = seqNum
+		current_response = clnt_sock.recv(1)[0]
+
 
 file.close() #파일 닫아줌
 clnt_sock.close() #socket닫아줌
+print("File send end.")
