b227571ecf0a0dd6269e8958283ecf0ffd9be57b
Suyeon Jin <gmlakd8944@cnu.ac.kr>
2018-05-23 17:21:44 +0900

    add normal operation(ACK)

---
+++ go-back-n/receiver.py
@@ -0,0 +1,95 @@
+import socket
+import struct
+import os
+import hashlib
+
+ip_address = '127.0.0.1'
+port_number = 3333
+
+def make_checksum(data): #checksum을 생성하는 함수
+	checksum = hashlib.sha1() #160bit 해시 암호
+	checksum.update(data)
+	return checksum
+
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_sock.bind((ip_address, port_number))
+
+print("Server socket open...")
+print("Send file info ACK..")
+
+while True:
+	data,addr = server_sock.recvfrom(1045)
+	output_checksum = data[0:20] #받은 암호 저장
+	output_data = data[20:]
+
+	#받은 데이터 checksum 생성
+	data_checksum = make_checksum(output_data)
+
+	output_seqNum = output_data[0]>>4 #4bit
+	output_ack = (output_data[0]&15) #4bit
+	if (output_checksum == data_checksum.digest()) and (output_seqNum == 0): #정상전송
+		file_name = output_data[1:12].decode()
+		file_size = struct.unpack("!I",output_data[12:16])[0]
+		file_path = "./received_dir/"
+
+		if not os.path.isdir("received_dir/"): #디렉토리 유무 확인
+			print("received_dir 디렉토리가 없으므로 생성합니다.")
+			os.mkdir("received_dir/") #없으면 생성
+
+		file = open(file_path+file_name,"wb")
+
+		print("File Name = ",file_name)
+		print("File Size = ",file_size)
+		print("received file Path = ",file_path+file_name)
+
+		prev_ack = output_ack #seqNum검사 위해 저장
+
+		output_seqNum = (output_seqNum+1)%8
+		output_ack = (output_ack+1)%8
+		bSeqNum = output_seqNum << 4 #4bit
+		ACK = output_ack & 0b1111 #4bit
+		
+		server_sock.sendto((bSeqNum|ACK).to_bytes(1,byteorder = "big"), addr)
+		break #정확한 정보를 받으면 종료
+
+
+#파일 데이터 받기
+current_size = 0 #만들어지는 파일의 현재 상태 확인하기 위한 변수
+receive_count = 0 #오류검사를 하기 위한 변수
+while True:
+	total_message,addr = server_sock.recvfrom(1045)
+	output_checksum = total_message[0:20] #checksum
+	output_seqNum = total_message[20]>>4 #4bit
+	output_ack = (total_message[20]&15) #4bit
+	output_message = total_message[21:]
+
+	#받은 데이터 checksum 생성
+	data_checksum = make_checksum(total_message[20:])
+	
+
+	check_ack = (prev_ack+1)%8
+	if(output_checksum == data_checksum.digest()) and (check_ack == output_seqNum): #정상전송(checksum검사 일치)
+		#파일 쓰기 시작
+		file.write(output_message)
+		current_size += len(output_message)
+		receive_count += 1 #파일을 몇번 receive하는지 저장
+
+		resend_message = "(current size / total size) = "+ str(current_size)+ "/"+ str(file_size)+ " , "+  str(round(100 * (current_size / file_size), 3))+ " %"
+
+		print(resend_message)
+
+		prev_ack = output_ack #seqNum검사 위해 저장
+
+		output_seqNum = (output_seqNum+1)%8
+		output_ack = (output_ack+1)%8
+		bSeqNum = output_seqNum << 4 #4bit
+		ACK = output_ack & 0b1111 #4bit
+		
+		server_sock.sendto((bSeqNum|ACK).to_bytes(1,byteorder = "big"), addr) #seqNum+ACK 전송
+
+	if current_size==file_size : #종료조건
+		break
+
+file.close()
+server_sock.close()
+print("File receive end.")
---
+++ go-back-n/sender.py
@@ -0,0 +1,126 @@
+import socket
+import struct
+import os
+import hashlib
+
+serverIP = '127.0.0.1'
+serverPort = 3333
+
+def make_checksum(data): #checksum을 생성하는 함수
+	checksum = hashlib.sha1() #160bit 해시 암호
+	checksum.update(data)
+	return checksum
+
+clnt_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP통신
+
+print("Sender Socket open..")
+print("Receiver IP = ",serverIP)
+print("Receiver Port = ", serverPort)
+print("Send File Info(file Name, file Size, seqNum) to Server...")
+
+#파일 정보(파일명,파일크기) 전송 : 초기설정
+file_name = "ex.jpg"
+
+# file open for read
+file = open("./"+file_name, "rb")
+
+file_size = os.path.getsize(file_name) #파일 크기 구하기
+file_name = file_name.ljust(11) #파일 이름을 11byte로 왼쪽정렬
+
+window=[0,0,0,0] #window 초기설정(size = 4)
+seqNum = 0 #초기설정(초기 seqNum는 0으로 설정)
+ACK = seqNum #ACK은 seqNum과 동일
+
+bSeqNum = seqNum << 4 #4bit
+ACK = ACK & 0b1111 #4bit
+
+total_data = (bSeqNum|ACK).to_bytes(1,byteorder = "big") + file_name.encode() + file_size.to_bytes(4,byteorder = "big")
+
+#checksum 생성
+checksum = make_checksum(total_data)
+
+#sending
+window[0] = checksum.digest()+total_data
+clnt_sock.sendto(window[0], (serverIP, serverPort))
+prev_seqNum = seqNum
+
+#파일 데이터 읽어서 보내기
+print("Start File send")
+current_size = 0 #만들어지는 파일의 현재 상태 확인하기 위한 변수
+
+for i in range(3): #처음 4개의 데이터는 연속으로 보냄
+	send_data = file.read(1024) #파일을 읽음
+
+	seqNum = seqNum+1 #seqNum update
+	seqNum = seqNum % 8 #0~7사이의 seqNum이 나오게 함
+	ACK = seqNum #ACK은 seqNum과 동일
+
+	bSeqNum = seqNum << 4 #4bit
+	ACK = ACK & 0b1111 #4bit
+
+	send_message = (bSeqNum|ACK).to_bytes(1,byteorder = "big") + send_data
+				
+	#checksum 생성
+	checksum = make_checksum(send_message)
+
+	#sending
+	window[i+1] = checksum.digest()+send_message
+	clnt_sock.sendto(window[i+1], (serverIP, serverPort))
+
+	current_size += len(send_data)
+
+	resend_message = "(current size / total size) = "+ str(current_size)+ "/"+ str(file_size)+ " , "+  str(round(100 * (current_size / file_size), 3))+ " %"
+	print(resend_message)
+
+	prev_seqNum = seqNum
+
+prev_ACK = 0 #초기값 설정
+while True:
+	if current_size == file_size: #마지막 검사
+		while True:
+			if (seqNum+1)%8 == recv_ACK: #종료조건 
+				break
+			response = clnt_sock.recv(1)[0]
+			recv_ACK = response & 15
+			recv_seqNum = response >> 4
+		break
+
+	response = clnt_sock.recv(1)[0]
+	recv_ACK = response & 15
+	recv_seqNum = response >> 4
+
+	if recv_ACK == 0:
+		cal_ACK = 8
+	else:
+		cal_ACK = recv_ACK
+
+	for i in range(cal_ACK - prev_ACK):
+		current_window = (prev_ACK) % 4	
+		send_data = file.read(1024) #파일을 읽음
+
+		seqNum = seqNum+1 #seqNum update
+		seqNum = seqNum % 8 #0~7사이의 seqNum이 나오게 함
+		ACK = seqNum #ACK은 seqNum과 동일
+		bSeqNum = seqNum << 4 #4bit
+		ACK = ACK & 0b1111 #4bit
+		
+		send_message = (bSeqNum|ACK).to_bytes(1,byteorder = "big") + send_data
+			
+		#checksum 생성
+		checksum = make_checksum(send_message)
+
+		#sending
+		window[current_window] = checksum.digest()+send_message
+		clnt_sock.sendto(window[current_window], (serverIP, serverPort))
+		current_size += len(send_data)
+
+		resend_message = "(current size / total size) = "+ str(current_size)+ "/"+ str(file_size)+ " , "+  str(round(100 * (current_size / file_size), 3))+ " %"
+		print(resend_message)
+
+		prev_seqNum = recv_seqNum
+		prev_ACK = (prev_ACK+1)%8
+
+
+file.close() #파일 닫아줌
+clnt_sock.close() #socket닫아줌
+print("File send end.")
